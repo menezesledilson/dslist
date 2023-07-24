2c91b6539ef760e18a0ebfabd92588e0eebaeda8
menezesledilson <ledilsonmenezes@gmail.com>
2023-07-24 14:53:39 -0300

    Deploy ready

--- dslist/src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ dslist/src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -1,9 +1,15 @@
 package com.devsuperior.dslist.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
-
+	
+	
+	
 }
